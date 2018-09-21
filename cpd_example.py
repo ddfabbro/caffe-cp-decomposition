146034f994b6ff0137de3abd54b40789c1cefc2b
Davi <fabda01@gmail.com>
2018-09-21 19:16:27 -0300

    Use OrderedDict for layer ranks

--- cpd_example.py
+++ cpd_example.py
@@ -1,26 +1,27 @@
-import cnn_cpd as cpd
-import urllib
 import os
+import urllib
+from collections import OrderedDict
+import cnn_cpd as cpd
 
-ROOT_DIR = '/path/to/cp_decomposition/'
+ROOT_DIR = '/path/to/CNN_CPD/'
+os.chdir(ROOT_DIR)
 
 if not os.path.isfile('models/VGG_ILSVRC_16_layers_deploy.caffemodel'):
     caffemodel_url = 'http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel'
     urllib.urlretrieve (caffemodel_url, 'models/VGG_ILSVRC_16_layers_deploy.caffemodel')
 
 model = {
-    'def': os.path.join(ROOT_DIR,'models/VGG_ILSVRC_16_layers_deploy.prototxt'),
-    'weights': os.path.join(ROOT_DIR,'models/VGG_ILSVRC_16_layers_deploy.caffemodel'),
+    'def': 'models/VGG_ILSVRC_16_layers_deploy.prototxt',
+    'weights': 'models/VGG_ILSVRC_16_layers_deploy.caffemodel',
 }
 
-layer_ranks = {
-    'conv3_1': 5,
-    'conv3_2': 5,
-    'conv3_3': 5,
-    'conv4_1': 5,
-    'conv4_2': 5,
-    'conv4_3': 5,
-}
+layer_ranks = OrderedDict([
+    ('conv4_1', 175),
+    ('conv4_2', 192),
+    ('conv4_3', 227),
+    ('conv5_1', 398),
+    ('conv5_2', 390),
+    ('conv5_3', 379),
+])
 
-new_model_def_path = cpd.decompose_model_def(model['def'], layer_ranks)
-cpd.decompose_model_weights(model['def'], model['weights'], new_model_def_path, layer_ranks)
+cpd_data, _ = cpd.decompose_model(model['def'], model['weights'], layer_ranks)
